525990a087382055636cefdc5c3fdd0b9b8ab511
syahmifirdaus <77308211+syahmifirdaus@users.noreply.github.com>
2021-03-10 13:58:44 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// var nav = document.getElementsByTagName(nav);
+
+
+function listTagLink() {
+    let navigationLinks = document.getElementsByTagName("nav");
+    console.log(navigationLinks);
+}
+
+function changeToDarkMode() {
+    let myElement = document.querySelector("");
+    myElement.style.backgroundColor = "#D93600";
+}
+
+function alert1() {
+    alert("Hi My name Syahmi Firdaus");
+}
